32817f7fe410dae7b2140153e795a0c5f0224cee
Hannah Kang <kyouyatamax@gmail.com>
2019-02-01 02:36:34 -0800

    generation object creates one generation of dragon then quits

--- generation.js
+++ generation.js
@@ -1,7 +1,7 @@
-const Corgi = require('./corgi');
-const { REFRESH_RATE, SECONDS} = require('./config');
+const Corgi = require("./corgi");
+const { REFRESH_RATE, SECONDS } = require("./config");
 
-const refreshRate = REFRESH_RATE*SECONDS;
+const refreshRate = REFRESH_RATE * SECONDS;
 
 class Generation {
   constructor() {
@@ -10,20 +10,20 @@ class Generation {
   calculateExpiration() {
     this.expiration = null;
 
-    const expirationPeriod = Math.floor(Math.random() * (refreshRate/2));
+    const expirationPeriod = Math.floor(Math.random() * (refreshRate / 2));
 
-    const msUntilExpiration = math.random() <0.5 ?
-      refreshRate - expirationPeriod : 
-      refreshRate + expirationPeriod;
-    return  new Date(Date.now() + msUntilExpiration);
+    const msUntilExpiration =
+      Math.random() < 0.5
+        ? refreshRate - expirationPeriod
+        : refreshRate + expirationPeriod;
+    return new Date(Date.now() + msUntilExpiration);
   }
 
   newCorgi() {
-    if (Date.now()> this.expiration) {
+    if (Date.now() > this.expiration) {
       throw new Error(`This generation expired on ${this.expiration}`);
     }
     return new Corgi();
-
   }
 }
 module.exports = Generation;
--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const Corgi = require('./corgi.js');
+const Generation = require('./generation');
+const generation = new Generation();
+console.log('generation', generation);
+const gooby = generation.newCorgi();
+console.log('gooby', gooby);
 
-const fooey = new Corgi({
-  birthdate: new Date(),
-  nickname: 'fooey'
-});
-const baloo = new Corgi({
-  birthdate: new Date(),
-  nickname: 'baloo'
-});
 
-const popop = new Corgi();
 setTimeout(() => {
-  const gooby = new Corgi();
-  console.log('gooby', gooby);
-},3000);
-console.log('fooey', fooey);
-console.log('baloo', baloo);
-console.log('popop', popop);
+  const mimar = generation.newCorgi();
+  console.log('mimar', mimar);
+
+},15000);
+
+// const Corgi = require('./corgi.js');
+
+// const fooey = new Corgi({
+//   birthdate: new Date(),
+//   nickname: 'fooey'
+// });
+// const baloo = new Corgi({
+//   birthdate: new Date(),
+//   nickname: 'baloo'
+// });
+
+// const popop = new Corgi();
+// setTimeout(() => {
+//   const gooby = new Corgi();
+//   console.log('gooby', gooby);
+// },3000);
+// console.log('fooey', fooey);
+// console.log('baloo', baloo);
+// console.log('popop', popop);
