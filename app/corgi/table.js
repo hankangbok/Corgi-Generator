11b351ab1681e70e8b8a670c884e89130117d3bd
Hannah Kang <kyouyatamax@gmail.com>
2019-02-28 00:44:58 -0800

    successfully inserted corgi traits into the table and can access using psql

--- app/corgi/table.js
+++ app/corgi/table.js
@@ -1,9 +1,8 @@
 const pool = require('../../databasePool');
+const CorgiTraitTable = require('../corgiTrait/table');
 class CorgiTable {
   static storeCorgi(corgi) {
     const {birthdate, nickname, generationId } = corgi;
-    
-
     return new Promise((resolve, reject) => {
       pool.query(
         `INSERT INTO corgi(birthdate, nickname, "generationId") 
@@ -12,7 +11,14 @@ class CorgiTable {
          (error, response) => {
            if (error) return reject(error);
            const corgiId = response.rows[0].id;
-           resolve({corgiId});
+
+           Promise.all( corgi.traits.map(({traitType,traitValue})=> {
+            return CorgiTraitTable.storeCorgiTrait({
+              corgiId, traitType, traitValue
+            });
+           }))
+            .then(()=> resolve({corgiId}))
+            .catch(error=> reject(error));
          }
       )
     });
