7263b86972c9dda2269d65acdb6ad44d42e84eb1
Hannah Kang <kyouyatamax@gmail.com>
2019-02-27 16:24:24 -0800

    added databaseConfiguration

--- bin/configure_db.sh
+++ bin/configure_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+#export PGPASSWORD=node_password'
 echo "Configuring corgi stack db"
 dropdb -U node_user corgistackdb
-createdb -U node_user dragonstackdb
-
+createdb -U node_user corgistackdb
 psql -U node_user corgistackdb < ./bin/sql/generation.sql
 psql -U node_user corgistackdb < ./bin/sql/corgi.sql
 echo "done Configuring corgi stack db"
---
+++ databasePool.js
@@ -0,0 +1,5 @@
+const {Pool} = require('pg');
+const databaseConfiguration = require('./secrets/databaseConfiguration');
+const pool = new Pool(databaseConfiguration);
+
+module.expots = pool;
